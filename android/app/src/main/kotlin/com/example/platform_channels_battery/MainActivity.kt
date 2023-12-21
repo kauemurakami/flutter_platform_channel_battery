699bec0d4a8e6ae4286ef7339118f34d97539cf2
Kauê <kauetmurakami@gmail.com>
2023-12-21 18:03:05 -0300

    finish kotlin

---
+++ android/app/src/main/kotlin/com/example/platform_channels_battery/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.example.platform_channels_battery
+
+import androidx.annotation.NonNull // Importe esta linha
+import io.flutter.embedding.engine.FlutterEngine // Importe esta linha
+import io.flutter.embedding.android.FlutterActivity
+
+import io.flutter.plugin.common.MethodChannel // Importe esta linha
+
+import android.content.Context
+import android.content.ContextWrapper
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.BatteryManager
+import android.os.Build.VERSION
+import android.os.Build.VERSION_CODES
+
+class MainActivity: FlutterActivity() {
+    private val CHANNEL = "samples.flutter.dev/battery"
+
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+         // Cria um canal de comunicação entre o código Dart e o código nativo (Kotlin/Java).
+        // O nome do canal é "samples.flutter.dev/battery".
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
+             // Este bloco de código é chamado quando o Dart envia uma mensagem para o canal.
+            // Verifica se a mensagem do Dart é para obter o nível da bateria.
+            if (call.method == "getBatteryLevel") {
+                val batteryLevel = getBatteryLevel()
+
+                if (batteryLevel != -1) {
+                    result.success(batteryLevel)
+                } else {
+                    // Se não for bem-sucedido, envia um erro de volta para o Dart.
+                    result.error("UNAVAILABLE", "Battery level not available.", null)
+                }
+            } else {
+              // Se a mensagem do Dart não for reconhecida, informa que não foi implementada.
+                result.notImplemented()
+            }
+        }
+    }
+
+    // Função para obter o nível da bateria.
+    private fun getBatteryLevel(): Int {
+        // Declara uma variável para armazenar o nível da bateria.
+        val batteryLevel: Int
+        // Verifica a versão do Android para escolher o método 
+        // adequado para obter o nível da bateria.
+        if (VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP) {
+            // Se a versão for igual ou superior ao Android Lollipop, usa a API BatteryManager.
+            val batteryManager = getSystemService(Context.BATTERY_SERVICE) as BatteryManager
+            batteryLevel = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)
+        } else {
+            // Se a versão for inferior ao Android Lollipop, usa a Intent ACTION_BATTERY_CHANGED.
+            val intent = ContextWrapper(applicationContext).registerReceiver(null, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
+            batteryLevel = intent!!.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) * 100 / intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1)
+        }
+        // Retorna o nível da bateria.
+        return batteryLevel
+    }
+}
---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  static const platform = MethodChannel('samples.flutter.dev/battery');
+  // Recupera o nivel de bateria.
+  String _batteryLevel = 'Unknown battery level.';
+
+  Future<void> _getBatteryLevel() async {
+    String batteryLevel;
+    try {
+      final result = await platform.invokeMethod<int>('getBatteryLevel');
+      batteryLevel = 'Battery level at $result % .';
+    } on PlatformException catch (e) {
+      batteryLevel = "Failed to get battery level: '${e.message}'.";
+    }
+
+    setState(() {
+      _batteryLevel = batteryLevel;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            ElevatedButton(
+              onPressed: _getBatteryLevel,
+              child: const Text('Get Battery Level'),
+            ),
+            Text(_batteryLevel),
+          ],
+        ),
+      ),
+    );
+  }
+}
